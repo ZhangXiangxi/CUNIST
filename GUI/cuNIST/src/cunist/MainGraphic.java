51e51bd1def1c23818c38e71a7c0def5434d119c
feihuanwuying <1397645429@qq.com>
2015-12-03 11:42:48 +0800

    完善GUI界面

--- GUI/cuNIST/src/cunist/MainGraphic.java
+++ GUI/cuNIST/src/cunist/MainGraphic.java
@@ -13,8 +13,7 @@ public class MainGraphic extends JFrame {
     private static int n = 10;
     private JPanel rootPanel;
     private WriteCanvas writeCanvas;  //手写
-    private DotMatrixCanvas dotMatrixCanvas;  //点阵
-    private ResultCanvas resultCanvas;  //结果
+    private DotMatrixCanvas dotMatrixCanvas;  //点
     JLabel resultJLabel;  //结果
     JLabel[] numberJLabels;  //十个数字
     JLabel[] proJLabels;  //概率
@@ -27,7 +26,7 @@ public class MainGraphic extends JFrame {
         this.addComponents();
         this.addListener();
         this.setContentPane(rootPanel);
-        this.setBounds(70, 150, 1200, 388);
+        this.setBounds(170, 150, 900, 388);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setVisible(true);
@@ -42,7 +41,6 @@ public class MainGraphic extends JFrame {
         rootPanel = new JPanel();
         dotMatrixCanvas = new DotMatrixCanvas();
         writeCanvas = new WriteCanvas(dotMatrixCanvas, this);
-        resultCanvas = new ResultCanvas();
         resultJLabel = new JLabel("?", JLabel.CENTER);
         proJLabels = new JLabel[n];
         numberJLabels = new JLabel[n];
@@ -56,19 +54,19 @@ public class MainGraphic extends JFrame {
     private void setAttr() {
         rootPanel.setLayout(null);
         writeCanvas.setBounds(5, 5, 350, 350);
-        dotMatrixCanvas.setBounds(356, 76, 280, 280);
-        clearButton.setBounds(370, 10, 120, 55);
+        dotMatrixCanvas.setBounds(406, 76, 280, 280);
+        clearButton.setBounds(420, 10, 120, 55);
         clearButton.setFocusPainted(false);
         clearButton.setContentAreaFilled(false);
         clearButton.setFont(new Font("楷体", Font.BOLD, 25));
         clearButton.setBorder(BorderFactory.createLineBorder(Color.RED));
-        resultJLabel.setBounds(545, 5, 70, 70);
+        resultJLabel.setBounds(595, 5, 70, 70);
         resultJLabel.setFont(new Font("黑体", Font.BOLD, 60));
         resultJLabel.setBorder(BorderFactory.createLineBorder(Color.blue));
         for (int i = 0; i < n; i++) {
-            numberJLabels[i].setBounds(640, 5+i*35, 30, 30);
+            numberJLabels[i].setBounds(740, 5+i*35, 30, 30);
             numberJLabels[i].setFont(new Font("黑体", Font.BOLD, 30));
-            proJLabels[i].setBounds(675, 5+i*35, 100, 30);
+            proJLabels[i].setBounds(775, 5+i*35, 100, 30);
             proJLabels[i].setFont(new Font("黑体", Font.PLAIN, 14));
         }
     }
